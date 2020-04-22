788ecfcf964125ba37cf3ade6c7c80087a8a0ace
Satyakam Goswami <satyaakam@gmail.com>
2020-04-22 18:24:22 +0530

    adding new version of Pelican

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,15 +2,15 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = u'Satyakam Goswami'
-SITENAME = u'Satyaakam.net'
+AUTHOR = 'Satyakam Goswami'
+SITENAME = 'My Personal blog'
 SITEURL = ''
 
 PATH = 'content'
 
-TIMEZONE = 'Asia/Kolkata'
+TIMEZONE = 'Europe/Paris'
 
-DEFAULT_LANG = u'en'
+DEFAULT_LANG = 'en'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -22,37 +22,15 @@ AUTHOR_FEED_RSS = None
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),
          ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),)
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         )
 
-#Variable to set Theme
-THEME = './Flex'
-
-SITESUBTITLE ='Learning and Living'
-SITETAG = "Satyaakam.net"
-
-# Extra stylesheets, for bootstrap overrides or additional styling.
-STYLESHEET_FILES = ("pygment.css", "voidybootstrap.css",)
-
-# Put taglist at end of articles, and use the default sharing button implementation.
-CUSTOM_ARTICLE_FOOTERS = ("taglist.html", "sharing.html", )
-CUSTOM_SCRIPTS_ARTICLE = "sharing_scripts.html"
-
-# Default sidebar template. Omit this line for single column mode without sidebar.
-SIDEBAR = "sidebar.html"
-
-SOCIAL = (('Google+', 'http://plus.google.com/satyaakam'),
-        ('Twitter', 'https://twitter.com/satyaakam'),
-        ('LinkedIn', 'https://www.linkedin.com/in/satyaakam'),
-        ('BitBucket', 'http://bitbucket.org/satyaakam'),
-        ('GitHub', 'http://github.com/satyaakam'),
-        )
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 10
 
+THEME = './Flex'
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
-
-STATIC_PATHS = ['images', 'CNAME', 'id_rsa.pub.txt']
-JINJA_ENVIRONMENT = {
-    'extensions': ['jinja2.ext.i18n'],
-}
